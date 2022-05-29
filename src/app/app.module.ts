8dbdfb3c6363f7e5e67dfff0c93be9fe213230bb
Ana Croitoru <ana-maria.croitoru@nokia.com>
2022-05-29 17:55:52 +0300

    Implement gallery service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { galleryReducer } from './store/gallery.reducer';
+import { GalleryService } from './gallery/gallery.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     StoreModule.forRoot({ gallery: galleryReducer }),
   ],
-  providers: [],
+  providers: [GalleryService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
